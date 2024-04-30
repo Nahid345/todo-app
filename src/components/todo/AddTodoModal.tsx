0087e353f0d2906b45435200c36b093d8e5d1866
Nahid345 <nahidparvej07@gmail.com>
2024-04-30 23:31:56 +0600

    added delete function & toggle state

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -21,7 +21,10 @@ const AddTodoModal = () => {
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
+
+    const randomString = Math.random().toString(36).substring(2, 7);
     const taskDetails = {
+      id: randomString,
       title: task,
       discription: description,
     };
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,8 +1,10 @@
+import { useAppSelector } from "@/redux/features/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const todoContainer = () => {
+  const { todos } = useAppSelector((state) => state.todos);
   return (
     <div>
       <div className="flex justify-between mb-5">
@@ -11,10 +13,9 @@ const todoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl  p-5">
         <div className="bg-white p-5 w-full h-full  space-y-3 rounded-lg">
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
+          {todos.map((item) => (
+            <TodoCard {...item} key={item.id} />
+          ))}
         </div>
         {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center">
           <p>There is no task pending</p>
